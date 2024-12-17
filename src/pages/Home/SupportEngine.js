a716673f3ae1aadc68e4ef80e8948b773e6aa0a0
Khent Lloyd Cases <khentlloyd3@gmail.com>
2024-12-17 15:27:36 +0800

    Revert "local host 5000"
    This reverts commit c3d0497d2236b773c72d1b4b506642496030abb9.

--- src/pages/Home/SupportEngine.js
+++ src/pages/Home/SupportEngine.js
@@ -1,19 +1,15 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
-import io from 'socket.io-client';
+import React, { useState, useEffect, useRef } from 'react';
 import './SupportEngine.css';
-import Avatar from './Avatar'; // Import Avatar.js
+import Avatar from './Avatar';
 import translations from './translations';
-
-const socket = io('http://localhost:5000'); // Connect to the WebSocket server
 
 const SupportEngine = () => {
   const [messages, setMessages] = useState([]);
-  const [userMessage, setUserMessage] = useState('');
-  const [isOpen, setIsOpen] = useState(false);
-  const [showOptions, setShowOptions] = useState([]);
-  const [language, setLanguage] = useState('en');
-  const [isRealTimeChat, setIsRealTimeChat] = useState(false);
-  const chatEndRef = useRef(null);
+  const [userMessage, setUserMessage] = useState(''); 
+  const [isOpen, setIsOpen] = useState(false); 
+  const [showOptions, setShowOptions] = useState([]); 
+  const [language, setLanguage] = useState('en'); 
+  const chatEndRef = useRef(null); 
   const t = translations[language]; 
 
   // Auto-scroll to the bottom when messages are updated
@@ -64,31 +60,17 @@ const SupportEngine = () => {
         botResponse = updatedTranslations.paymentMethod;
         break;
 
-        case updatedTranslations.options[3]: // More
-        botResponse = t.more;
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { sender: 'bot', text: botResponse },
-        ]);
+      case updatedTranslations.options[3]: 
+        botResponse = updatedTranslations.more;
         setTimeout(() => setShowOptions(['Yes', 'No']), 500);
-        return;
+        break;
 
-      case 'Yes': // User chooses "Yes"
-        botResponse = t.yes;
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { sender: 'bot', text: botResponse },
-        ]);
-        setTimeout(() => {
-          setIsRealTimeChat(true);
-          socket.emit('notify admin', { message: 'A user needs real-time support!' });
-          setShowOptions([]);
-        }, 1000);
-        return;
+      case 'Yes':
+        botResponse = updatedTranslations.yes;
+        break;
 
-      case 'No': // User chooses "No"
-        botResponse = t.no;
-        setTimeout(() => setShowOptions(t.options), 500);
+      case 'No':
+        botResponse = updatedTranslations.no;
         break;
 
       case updatedTranslations.options[4]: 
